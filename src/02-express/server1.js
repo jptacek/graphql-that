0205251815b5958df0f014fec2d63cf52cee5618
John Ptacek <jptacek@gmail.com>
2022-07-24 14:03:26 -0500

    Stub out express server

---
+++ src/02-express/server1.js
@@ -0,0 +1,21 @@
+import { graphqlHTTP } from 'express-graphql';
+import { schema, rootValue } from './schema/index.js';
+import express, {json} from 'express';
+
+async function main() {
+  const server = express();
+  server.use(express.json());
+  const port =  process.env.PORT || 4000;
+
+  // Example route
+  server.use('/', (req, res) => {
+    res.send('Hello That Conference');
+  });
+
+  // This line rus the server
+  server.listen(port, () => {
+    console.log(`Server URL: http://localhost:${port}/`);
+  });
+}
+
+main();
